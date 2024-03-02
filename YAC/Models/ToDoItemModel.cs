37541d1a31f591ef0b3a68a5e077416a799f152e
travis@travestyofcode.com <travis@travestyofcode.com>
2024-03-02 13:24:36 -0500

    Adds view models for data models.

---
+++ YAC/Models/ToDoItemModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+
+namespace YAC.Models;
+
+public class ToDoItemModel
+{
+    public int Id { get; set; }
+
+    public int ToDoListId { get; set; }
+
+    public bool IsCompleted { get; set; }
+
+    public string Description { get; set; }
+
+    public DateTimeOffset? DueBy { get; set; }
+}
+
+public static class ToDoItemModelExtensions
+{
+    public static IQueryable<ToDoItemModel> ProjectToModel(this IQueryable<Data.ToDoItem> query)
+    {
+        return query?.Select(p => new ToDoItemModel()
+        {
+            Id = p.Id,
+            ToDoListId = p.ToDoListId,
+            IsCompleted = p.IsCompleted,
+            Description = p.Description,
+            DueBy = p.DueBy
+        });
+    }
+}
---
+++ YAC/Models/ToDoListModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace YAC.Models;
+
+public class ToDoListModel
+{
+    public int Id { get; set; }
+
+    public string OwnerId { get; set; }
+
+    public string Title { get; set; }
+
+    public IEnumerable<ToDoItemModel> Items { get; set; }
+}
+
+public static class ToDoListModelExtensions
+{
+    public static IQueryable<ToDoListModel> ProjectToModel(this IQueryable<Data.ToDoList> query)
+    {
+        return query?.Select(p => new ToDoListModel()
+        {
+            Id = p.Id,
+            OwnerId = p.OwnerId,
+            Title = p.Title,
+            Items = p.Items.AsQueryable().ProjectToModel()
+        });
+    }
+}
