c1797c08cdb5542536b31036c5eb615097d45e21
travis@travestyofcode.com <travis@travestyofcode.com>
2024-03-02 12:46:44 -0500

    Adds ToDo List and Item models, adds to DBContext and creates migration.

--- YAC/Data/Migrations/20240302070727_Intial-Data.Designer.cs
+++ YAC/Data/Migrations/20240302070727_Intial-Data.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using YAC.Data;
 
@@ -11,9 +12,11 @@ using YAC.Data;
 namespace YAC.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240302070727_Intial-Data")]
+    partial class IntialData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ YAC/Data/ToDoItem.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace YAC.Data;
+
+public class ToDoItem
+{
+    public int Id { get; set; }
+
+    public int ToDoListId { get; set; }
+
+    public ToDoList ToDoList { get; set; }
+
+    public bool IsCompleted { get; set; }
+
+    public string Description { get; set; }
+
+    public DateTimeOffset? DueBy { get; set; }
+}
+
+public class ToDoItemConfiguration : IEntityTypeConfiguration<ToDoItem>
+{
+    public void Configure(EntityTypeBuilder<ToDoItem> builder)
+    {
+        builder.ToTable(nameof(ToDoItem));
+
+        builder.HasKey(p => p.Id);
+
+        builder.Property(p => p.Description)
+            .HasMaxLength(256)
+            .IsRequired(true);
+
+        builder.Property(p => p.DueBy)
+            .IsRequired(false);
+
+        builder.HasOne(p => p.ToDoList)
+            .WithMany(p => p.Items)
+            .HasPrincipalKey(p => p.Id)
+            .HasForeignKey(p => p.ToDoListId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
