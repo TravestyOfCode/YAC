336b9c3bd4f9258ca426ca6c4fddc1e6aa317558
travis@travestyofcode.com <travis@travestyofcode.com>
2024-03-02 13:07:26 -0500

    Adds static string view names.

---
+++ YAC/Utilities/ViewNames.cs
@@ -0,0 +1,8 @@
+﻿namespace YAC.Utilities;
+
+public static class ViewNames
+{
+    public static readonly string Home = "Home";
+    public static readonly string Index = "Index";
+    public static readonly string Main = "Main";
+}
