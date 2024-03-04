3eeee4e7d79ec8c7b52edac326f94852bd74fa03
travis@travestyofcode.com <travis@travestyofcode.com>
2024-03-04 09:14:10 -0500

    Adds editable title actions/views.

--- YAC/Controllers/MainController.cs
+++ YAC/Controllers/MainController.cs
@@ -7,6 +7,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using YAC.Data;
 using YAC.Models;
+using YAC.Utilities;
 
 namespace YAC.Controllers;
 
@@ -43,5 +44,48 @@ public class MainController : Controller
 
             return StatusCode(500);
         }
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> EditingTitle(int id, CancellationToken cancellationToken)
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        var entity = await _dbContext.ToDoList
+            .Where(p => p.Id.Equals(id) && p.OwnerId.Equals(userId))
+            .ProjectToModel()
+            .SingleOrDefaultAsync(cancellationToken);
+
+        if (entity == null)
+        {
+            return NotFound();
+        }
+
+        return PartialView(ViewNames.EditingTitle, entity);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> EditingTitle(int id, string title, CancellationToken cancellationToken)
+    {
+        if (title == null || title.Length > 64)
+        {
+            return BadRequest();
+        }
+
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        var entity = await _dbContext.ToDoList
+            .SingleOrDefaultAsync(p => p.Id.Equals(id) && p.OwnerId.Equals(userId), cancellationToken);
+
+        if (entity == null)
+        {
+            return NotFound();
+        }
+
+        entity.Title = title;
+
+        await _dbContext.SaveChangesAsync(cancellationToken);
+
+        return PartialView(ViewNames.EditableTitle, entity.AsModel());
     }
 }
--- YAC/Utilities/ViewNames.cs
+++ YAC/Utilities/ViewNames.cs
@@ -5,4 +5,7 @@ public static class ViewNames
     public static readonly string Home = "Home";
     public static readonly string Index = "Index";
     public static readonly string Main = "Main";
+
+    public static readonly string EditableTitle = "EditableTitle";
+    public static readonly string EditingTitle = "EditingTitle";
 }
